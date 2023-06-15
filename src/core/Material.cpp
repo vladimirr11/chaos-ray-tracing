bab1d85b25d01ead2ee334308f1d8668d79a969e
Vladimir Stoev <vladimirstoevv@gmail.com>
2023-06-15 23:14:34 +0300

    Removed unnecessary switch statements in shadeRefractive and shadeReflective functions.

--- src/core/Material.cpp
+++ src/core/Material.cpp
@@ -3,14 +3,14 @@
 
 Color3f Material::shade(const Ray& ray, const Scene* scene, Intersection& isectData) const {
     Color3f shadeColor;
-    const MaterialType hittedMaterial = scene->getMaterials()[isectData.materialIdx].type;
-    if (hittedMaterial == MaterialType::DIFFUSE) {
+    const MaterialType& hittedMaterialType = scene->getMaterials()[isectData.materialIdx].type;
+    if (hittedMaterialType == MaterialType::DIFFUSE) {
         shadeColor = shadeDiffuse(ray, scene, isectData);
-    } else if (hittedMaterial == MaterialType::REFLECTIVE) {
+    } else if (hittedMaterialType == MaterialType::REFLECTIVE) {
         shadeColor = shadeReflective(ray, scene, isectData);
-    } else if (hittedMaterial == MaterialType::REFRACTIVE) {
+    } else if (hittedMaterialType == MaterialType::REFRACTIVE) {
         shadeColor = shadeRefractive(ray, scene, isectData);
-    } else if (hittedMaterial == MaterialType::CONSTANT) {
+    } else if (hittedMaterialType == MaterialType::CONSTANT) {
         shadeColor = shadeConstant(ray, scene, isectData);
     } else {
         Assert(false && "Material::shade() received unsupported material type.");
@@ -43,23 +43,18 @@ Color3f shadeDiffuse([[maybe_unused]] const Ray& ray, const Scene* scene, Inters
 }
 
 Color3f shadeReflective(const Ray& ray, const Scene* scene, Intersection& isectData) {
-    const Vector3f reflectedDir = reflect(ray.dir, isectData.faceNormal);
-    Ray reflectedRay = Ray(isectData.pos + isectData.faceNormal * REFLECTION_BIAS, reflectedDir);
+    bool smoothShading = scene->getMaterials()[isectData.materialIdx].smoothShading;
+    Normal3f surfNormal = smoothShading ? isectData.smoothNormal : isectData.faceNormal;
+    const Vector3f reflectedDir = reflect(ray.dir, surfNormal);
+    Ray reflectedRay = Ray(isectData.pos + surfNormal * REFLECTION_BIAS, reflectedDir);
     const Color3f& albedo = scene->getMaterials()[isectData.materialIdx].property.albedo;
-    if (ray.depth < MAX_RAY_DEPTH && scene->intersect(reflectedRay, isectData)) {
-        switch (scene->getMaterials()[isectData.materialIdx].type) {
-        case MaterialType::REFLECTIVE:
+    if (ray.depth <= MAX_RAY_DEPTH && scene->intersect(reflectedRay, isectData)) {
+        const Material* hittedMaterial = &scene->getMaterials()[isectData.materialIdx];
+        if (hittedMaterial->type == MaterialType::REFLECTIVE) {
             reflectedRay.depth = ray.depth + 1;
-            return shadeReflective(reflectedRay, scene, isectData);
-        case MaterialType::DIFFUSE:
-            return albedo * shadeDiffuse(reflectedRay, scene, isectData);
-        case MaterialType::REFRACTIVE:
-            reflectedRay.depth = ray.depth + 1;
-            return albedo * shadeRefractive(reflectedRay, scene, isectData);
-        case MaterialType::CONSTANT:
-            return albedo * shadeConstant(reflectedRay, scene, isectData);
-        default:
-            Assert(false && "shadeReflective() received unsupported material type.");
+            return hittedMaterial->shade(reflectedRay, scene, isectData);
+        } else {
+            return albedo * hittedMaterial->shade(reflectedRay, scene, isectData);
         }
     }
     return albedo * scene->getBackground();
@@ -67,64 +62,57 @@ Color3f shadeReflective(const Ray& ray, const Scene* scene, Intersection& isectD
 
 Color3f shadeRefractive(const Ray& ray, const Scene* scene, Intersection& isectData) {
     if (ray.depth <= MAX_RAY_DEPTH) {
-        switch (scene->getMaterials()[isectData.materialIdx].type) {
-        case MaterialType::REFLECTIVE:
-        case MaterialType::DIFFUSE:
-            return shadeDiffuse(ray, scene, isectData);
-        case MaterialType::CONSTANT:
-            return shadeConstant(ray, scene, isectData);
-        case MaterialType::REFRACTIVE: {
-            bool smoothShading = scene->getMaterials()[isectData.materialIdx].smoothShading;
-            Normal3f surfNormal = smoothShading ? isectData.smoothNormal : isectData.faceNormal;
-            float cosThetaI = clamp(-1.f, 1.f, dot(ray.dir, surfNormal));
-            const float ior = scene->getMaterials()[isectData.materialIdx].property.ior;
-            float etaI = 1.f, etaT = ior;
-            bool rayLeaveTransparent = cosThetaI > 0.f;
-            if (rayLeaveTransparent) {
-                std::swap(etaI, etaT);
-                surfNormal = -surfNormal;
-            } else
-                cosThetaI = -cosThetaI;
+        bool smoothShading = scene->getMaterials()[isectData.materialIdx].smoothShading;
+        Normal3f surfNormal = smoothShading ? isectData.smoothNormal : isectData.faceNormal;
+        float cosThetaI = clamp(-1.f, 1.f, dot(ray.dir, surfNormal));
+        const float ior = scene->getMaterials()[isectData.materialIdx].property.ior;
+        float etaI = 1.f, etaT = ior;
+        bool rayLeaveTransparent = cosThetaI > 0.f;
+        if (rayLeaveTransparent) {
+            std::swap(etaI, etaT);
+            surfNormal = -surfNormal;
+        } else
+            cosThetaI = -cosThetaI;
 
-            Color3f refractColor, reflectColor;
-            Vector3f refrRayDir;
-            if (refract(ray.dir, surfNormal, etaI / etaT, cosThetaI, &refrRayDir)) {
-                Ray refractionRay =
-                    Ray(isectData.pos + (-surfNormal * REFRACTION_BIAS), refrRayDir);
-                Intersection refrIsectData;
-                if (scene->intersect(refractionRay, refrIsectData)) {
-                    refractionRay.depth = ray.depth + 1;
-                    refractColor = shadeRefractive(refractionRay, scene, refrIsectData);
-                } else {
-                    refractColor = scene->getBackground();
-                }
-
-                const Vector3f reflRayDir = reflect(ray.dir, surfNormal);
-                Ray reflectionRay = Ray(isectData.pos + (surfNormal * REFLECTION_BIAS), reflRayDir);
-                Intersection reflIsectData;
-                if (scene->intersect(reflectionRay, reflIsectData)) {
-                    reflectionRay.depth = ray.depth + 1;
-                    reflectColor = shadeRefractive(reflectionRay, scene, reflIsectData);
-                } else {
-                    reflectColor = scene->getBackground();
-                }
-
-                const float fres = fresnel(ray.dir, surfNormal);
-                return fres * reflectColor + (1 - fres) * refractColor;
+        Color3f refractColor, reflectColor;
+        Vector3f refrRayDir;
+        if (refract(ray.dir, surfNormal, etaI / etaT, cosThetaI, &refrRayDir)) {
+            // construct and trace refraction ray in the scene
+            Ray refractionRay = Ray(isectData.pos + (-surfNormal * REFRACTION_BIAS), refrRayDir);
+            Intersection refrIsectData;
+            if (scene->intersect(refractionRay, refrIsectData)) {
+                refractionRay.depth = ray.depth + 1;
+                const Material* hittedMaterial = &scene->getMaterials()[refrIsectData.materialIdx];
+                refractColor = hittedMaterial->shade(refractionRay, scene, refrIsectData);
             } else {
-                const Vector3f reflRayDir = reflect(ray.dir, surfNormal);
-                Ray reflectionRay = Ray(isectData.pos + (surfNormal * REFLECTION_BIAS), reflRayDir);
-                Intersection reflIsectData;
-                if (scene->intersect(reflectionRay, reflIsectData)) {
-                    reflectionRay.depth = ray.depth + 1;
-                    return shadeRefractive(reflectionRay, scene, reflIsectData);
-                } else {
-                    return scene->getBackground();
-                }
+                refractColor = scene->getBackground();
             }
-        }
-        default:
-            Assert(false && "shadeRefractive() received unsupported material type.");
+
+            // construct and trace reflection ray in the scene
+            const Vector3f reflRayDir = reflect(ray.dir, surfNormal);
+            Ray reflectionRay = Ray(isectData.pos + (surfNormal * REFLECTION_BIAS), reflRayDir);
+            Intersection reflIsectData;
+            if (scene->intersect(reflectionRay, reflIsectData)) {
+                reflectionRay.depth = ray.depth + 1;
+                const Material* hittedMaterial = &scene->getMaterials()[reflIsectData.materialIdx];
+                reflectColor = hittedMaterial->shade(reflectionRay, scene, reflIsectData);
+            } else {
+                reflectColor = scene->getBackground();
+            }
+
+            const float fres = fresnel(ray.dir, surfNormal);
+            return fres * reflectColor + (1 - fres) * refractColor;
+        } else {
+            const Vector3f reflRayDir = reflect(ray.dir, surfNormal);
+            Ray reflectionRay = Ray(isectData.pos + (surfNormal * REFLECTION_BIAS), reflRayDir);
+            Intersection reflIsectData;
+            if (scene->intersect(reflectionRay, reflIsectData)) {
+                reflectionRay.depth = ray.depth + 1;
+                const Material* hittedMaterial = &scene->getMaterials()[reflIsectData.materialIdx];
+                return hittedMaterial->shade(reflectionRay, scene, reflIsectData);
+            } else {
+                return scene->getBackground();
+            }
         }
     }
 
