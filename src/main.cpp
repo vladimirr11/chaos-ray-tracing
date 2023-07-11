de35d5063ce1afc44078c3dd603b5d357bf9feb9
Vladimir Stoev <vladimirstoevv@gmail.com>
2023-07-11 22:11:36 +0300

    Restored main and runRenderer functions.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,15 @@
 
 // #define RENDER_STATIC
 
-static int32_t runRenderer(const std::string& inputFile, ThreadPool& pool, RenderSettings& settings,
-                           const int32_t numFrames) {
-    // parse scene parameters
+static int32_t runRenderer(const std::string& inputFile, ThreadPool& pool,
+                           RenderSettings& settings) {
+    const std::string ppmFileName = getPpmFileName(inputFile);
+    std::ofstream ppmImageFile(ppmFileName, std::ios::out | std::ios::binary);
+    if (!ppmImageFile.good()) {
+        std::cout << "Input file " << inputFile << " not good.\n";
+        return EXIT_FAILURE;
+    }
+
     SceneParams sceneParams;
     if (parseSceneParams(inputFile, sceneParams) != EXIT_SUCCESS) {
         std::cerr << "Failed to parse " << inputFile << " file." << std::endl;
@@ -17,80 +23,60 @@ static int32_t runRenderer(const std::string& inputFile, ThreadPool& pool, Rende
 
     // initialize scene
     Scene scene(sceneParams);
-    scene.createAccelTree();
 
+    // initialize image
     const SceneDimensions dimens = scene.getSceneDimensions();
+    PPMImageI ppmImage(dimens.width, dimens.height);
+
+    // initialize renderer
+    Renderer renderer(ppmImage, &scene);
+
     settings.numPixelsPerThread = scene.getSceneSettings().bucketSize;
 
-    Camera& sceneCamera = scene.getCamera();
-
-    const char* scenePrefix = {"scene"};
-    int32_t moveCameraCloser = -1;
-    const int32_t switchFrame = 43;
-    for (int32_t i = 0; i < numFrames; i++) {
-        const std::string fileName = scenePrefix + std::to_string(i) + ".ppm";
-        std::ofstream ppmImageFile(fileName, std::ios::out | std::ios::binary);
-        if (!ppmImageFile.good()) {
-            std::cout << "Input file " << inputFile << " not good.\n";
-            return EXIT_FAILURE;
-        }
-
-        if (i % switchFrame == 0)
-            ++moveCameraCloser;
-        moveCameraCloser % 2 == 0
-            ? sceneCamera.move(Vector3f{0.55f, 0.f, -0.5f})  // move camera closer
-            : sceneCamera.move(Vector3f{0.55f, 0.f, 0.5f});  // move camera away
-
-        sceneCamera.setLookAt(Vector3f{0.f, 0.f, 0.f});
-
-        // initialize image and renderer
-        PPMImageI ppmImage(dimens.width, dimens.height);
-        Renderer renderer(ppmImage, &scene);
-        {
-            std::cout << "Loading " << fileName << " scene...\nStart generating data...\n";
-            Timer timer;
-            timer.start();
+    std::cout << "Loading " << ppmFileName << " scene...\n";
+    scene.createAccelTree();
+    {
+        std::cout << "Start generating data...\n";
+        Timer timer;
+        timer.start();
 
 #ifdef RENDER_STATIC
-            for (size_t threadId = 0; threadId < settings.numThreads; threadId++) {
-                auto renderTask = std::bind(&Renderer::renderStatic, &renderer, threadId,
-                                            settings.numThreads, settings.numPixelsPerThread);
-                pool.scheduleTask(renderTask);
-            }
-#else
-            using namespace std::placeholders;
-            auto renderTask = std::bind(&Renderer::renderRegion, &renderer, _1, _2, _3, _4);
-            pool.parallelLoop2D(renderTask, (size_t)dimens.width, (size_t)dimens.height,
-                                settings.numPixelsPerThread, settings.numPixelsPerThread);
-#endif
-            pool.completeTasks();
-
-            std::cout << fileName << " data generated in [" << std::fixed << std::setprecision(2)
-                      << Timer::toMilliSec<float>(timer.getElapsedNanoSec()) << "ms] on "
-                      << settings.numThreads << " threads\n";
-
-            flushStatistics();
+        for (size_t threadId = 0; threadId < settings.numThreads; threadId++) {
+            auto renderTask = std::bind(&Renderer::renderStatic, &renderer, threadId,
+                                        settings.numThreads, settings.numPixelsPerThread);
+            pool.scheduleTask(renderTask);
         }
+#else
+        using namespace std::placeholders;
+        auto renderTask = std::bind(&Renderer::renderRegion, &renderer, _1, _2, _3, _4);
+        pool.parallelLoop2D(renderTask, (size_t)dimens.width, (size_t)dimens.height,
+                            settings.numPixelsPerThread, settings.numPixelsPerThread);
+#endif
+        pool.completeTasks();
 
-        serializePPMImage(ppmImageFile, ppmImage);
-        ppmImageFile.close();
+        std::cout << ppmFileName << " data generated in [" << std::fixed << std::setprecision(2)
+                  << Timer::toMilliSec<float>(timer.getElapsedNanoSec()) << "ms] on "
+                  << settings.numThreads << " threads\n";
+
+        flushStatistics();
     }
+
+    serializePPMImage(ppmImageFile, ppmImage);
+    ppmImageFile.close();
 
     return EXIT_SUCCESS;
 }
 
 int main() {
-    const std::vector<std::string> inputFiles = {"scene1.crtscene"};
+    const std::vector<std::string> inputFiles = {"scenes/scene1.crtscene"};
 
     RenderSettings renderSettings;
 
     ThreadPool pool(renderSettings.numThreads);
     pool.start();
 
-    const int32_t numFrames = 180;
-
     for (const auto& file : inputFiles) {
-        if (runRenderer(file, pool, renderSettings, numFrames) != EXIT_SUCCESS) {
+        if (runRenderer(file, pool, renderSettings) != EXIT_SUCCESS) {
             std::cerr << "Failed to render file - " << file << std::endl;
             return EXIT_FAILURE;
         }
